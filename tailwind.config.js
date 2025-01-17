a80535aaeacf8a0f16dc2515b847e3980c514b1b
Dpaace <dipeshnepali767@gmail.com>
2025-01-17 11:57:11 +0545

    Navbar somewhat kind of fixed for now but feedback is needed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./*.{html,js}",
-        './*.html',
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./*.{html,js}",
+    './*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+      },
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
