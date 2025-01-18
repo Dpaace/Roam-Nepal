e0a4f784057656844577ceea6bb0be018cc96801
Dpaace <dipeshnepali767@gmail.com>
2025-01-18 19:12:27 +0545

    Home Page Completed with Responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        clifford: '#da373d',
+        primary: '#03677E',
+        txtPrimary : '#03677E',
+        txtPlaceholder : '#49454F',
+        formText: '#0B3C49',
+        brcolor: '#03677E',
+        btnbg: '#03677E',
+        btnOutline: '#03677E',
+        bannerTxt: '#001A42',
+        bannerBorder: '#40A9FF',
+        txtStatistics: '#036773',
+        bgButton: '#0B3C49'
       },
       fontFamily: {
         sans: ['Open Sans', 'sans-serif'],
